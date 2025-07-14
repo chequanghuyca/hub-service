5fb2d56d93e41a5937eaf0b2cd6fc44ef24b390d
Che Quang Huy <chequanghuybtt@gmail.com>
2025-07-14 22:54:02 +0700

    chore: img on challenge

--- module/challenge/storage/get.go
+++ module/challenge/storage/get.go
@@ -17,3 +17,16 @@ func (s *Storage) Get(ctx context.Context, id primitive.ObjectID) (*model.Challe
 	}
 	return &data, nil
 }
+
+func (s *Storage) GetChallenge(ctx context.Context, id primitive.ObjectID) (*model.Challenge, error) {
+	var data model.Challenge
+
+	collection := s.db.MongoDB.GetCollection(model.CollectionName)
+	err := collection.FindOne(ctx, bson.M{"_id": id}).Decode(&data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &data, nil
+}
